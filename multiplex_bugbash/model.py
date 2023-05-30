11e108e187793c192c8212196366b700700c712a
Sihan Wang <sihanwang41@gmail.com>
2023-05-30 15:38:43 -0700

    sleep
    Signed-off-by: Sihan Wang <sihanwang41@gmail.com>

--- multiplex_bugbash/model.py
+++ multiplex_bugbash/model.py
@@ -9,6 +9,7 @@ class ModelInferencer:
     
     @serve.multiplexed(max_num_models_per_replica=5)
     async def get_model(self, model_id: str):
+        await asyncio.sleep(1)
         return model_id
 
     async def __call__(self, request: starlette.requests.Request):
